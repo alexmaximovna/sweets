15c55432b0916c1e564ef64970e0636269477082
alexandra <alex.maximovna@gmail.ru>
2019-09-23 19:30:48 +0600

    Adding color text

--- app/src/main/java/ru/imit/omsu/sweet/MainActivity.java
+++ app/src/main/java/ru/imit/omsu/sweet/MainActivity.java
@@ -2,20 +2,26 @@ package ru.imit.omsu.sweet;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.annotation.SuppressLint;
 import android.content.Context;
 import android.content.Intent;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
 import android.graphics.Color;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.TextView;
 
 import java.io.ByteArrayOutputStream;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Set;
 
 public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+
 
 
     @Override
@@ -31,6 +37,31 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         button3.setOnClickListener(this);
     }
 
+    @Override
+    protected void onSaveInstanceState(Bundle outState) {
+        super.onSaveInstanceState(outState);
+        TextView  t = findViewById(R.id.des_honey);
+        TextView  t1 = findViewById(R.id.des_zebra);
+        TextView  t2 = findViewById(R.id.des_peaches);
+        outState.putString("honey", String.valueOf(t.getCurrentTextColor()));
+        outState.putString("zebra", String.valueOf(t1.getCurrentTextColor()));
+        outState.putString("peaches", String.valueOf(t2.getCurrentTextColor()));
+    }
+
+    @Override
+    protected void onRestoreInstanceState(Bundle savedInstanceState) {
+            super.onRestoreInstanceState(savedInstanceState);
+        String str = savedInstanceState.getString("honey");
+        String str1 = savedInstanceState.getString("zebra");
+        String str2 = savedInstanceState.getString("peaches");
+        TextView  t = findViewById(R.id.des_honey);
+        TextView  t1 = findViewById(R.id.des_zebra);
+        TextView  t2 = findViewById(R.id.des_peaches);
+        t.setTextColor(Integer.parseInt(str));
+        t1.setTextColor(Integer.parseInt(str1));
+        t2.setTextColor(Integer.parseInt(str2));
+
+    }
     public void onClick(View view) {
         switch (view.getId()){
             case R.id.honey_details:
